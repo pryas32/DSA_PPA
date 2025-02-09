7fcf3c10584b443d4196e4e434df0ceb68373bce
pryas32 <shuklaaayush805@gmail.com>
2025-02-09 18:13:41 +0530

    Create datatypename.c
    final commit

---
+++ IntroductionToProgramming/datatypename/src/datatypename.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+
+
+int main(void){
+
+	// the way we learn first letter then combination of letter ie words
+	//similarly in programming language like C/c++
+	// we have variable b='s'; for storing the data
+//	and what type of data they store that we have have data type
+
+	// int a=10;signifies that a varible a is created in RAM and it holds 10 in it which is of type int 4 bytes allocated inside RAM.
+	//convention need to follow while decalring variable name in c etc
+	//should be letter(english alphabet including '$','#',digits(0 to n) and underscore
+	//but shouldnt have data type or keyword as data type
+
+//	int a=10;
+	//int a 23;//not alloweded
+
+
+	int a=23;
+
+
+
+
+
+
+	//int else=23;//cant use keyword for data type
+
+	int _a=223;
+
+
+	printf("%d",_a);
+
+	//cant have  int a b;
+	//collcetions of character called string
+	//String str="abc"; // @suppress("Type cannot be resolved")
+
+	printf("\n");
+
+int $a=34;
+
+printf("%d",$a);
+
+//naming convention
+//consist of letter(a-z,A-Z shoiuldnt be +a or something like that as they denote arthematic operator keywords slike return or if else etc
+//compiler makes differeence between int a,A;
+
+
+//cant have int -a=23; etc
+
+return 0;
+
+
+}
